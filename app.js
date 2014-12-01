c98af64e9c8cdd310bc45f86621c01aa8f8f41a6
Gastón Sánchez <gaastonsr@gmail.com>
2014-12-01 04:07:53 -0600

    Add CORS support

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var express      = require('express');
 var bodyParser   = require('body-parser');
 var config       = require('config');
+var cors         = require('cors');
 var routes       = require('./routes');
 var toolkit      = require('./libs/api-toolkit');
 var error404     = require('./middlewares/error404');
@@ -13,6 +14,7 @@ var app = express();
 app.set('env',  config.get('nodeEnv'));
 app.set('port', config.get('port'));
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(toolkit.energizer());
 app.use(routes);
